81b3bbabaf5bece9d65298a4a72ded755b817a6d
Megha1133 <163384342+Megha1133@users.noreply.github.com>
2024-05-22 22:30:15 +0530

    Add files via upload

---
+++ boehringer_theme 4/boehringer_theme/js/script.js
@@ -0,0 +1,34 @@
+// alert("Hi anil");
+
+// const hamMenu = document.querySelector('.ham-menuu');
+ 
+//  const offScreenMenu = document.querySelector('.off-screen-menu');
+ 
+//  hamMenu.addEventListener('click', () => {
+//     hamMenu.class.List.toggle('active');
+//     offScreenMenu.classList.toggle('active');
+//  })
+
+// function myFunction(){
+//     var x= document.getElementById("mobilemenutoggle");
+//     document.getElementsByClassName("gds-search__input").innerHTML= "You are searching for: " + x.value;
+// }
+
+// const search = document.querySelector('.gds-search')
+// const btn = document.querySelector('#mobilemenutoggle')
+// const input = document.querySelector('.gds-search__input')
+// btn.addEventListener('click', (event) => {
+// search.classList.toggle('active')
+// input.focus()
+// event.preventDefault();
+// })
+
+// const search = document.querySelector('.search')
+// const btn = document.querySelector('.btn')
+// const input = document.querySelector('.input')
+// btn.addEventListener('click', () => {
+// search.classList.toggle('active')
+// input.focus()
+// })
+
+// alert("hi");
