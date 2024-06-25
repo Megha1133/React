2478de25b0f3c93a6d4b622ddf8784de689d4465
Megha1133 <163384342+Megha1133@users.noreply.github.com>
2024-06-25 21:50:52 +0530

    Add files via upload

---
+++ bajaj_theme/js/script.js
@@ -0,0 +1,31 @@
+//alert("Hi megha");
+
+//const hamMenu = document.querySelector('.ham-menuu');
+ 
+//const offScreenMenu = document.querySelector('.off-screen-menu');
+ //hamMenu.addEventListener('click', () => {
+   //hamMenu.class.List.toggle('active');
+    //offScreenMenu.classList.toggle('active');
+//})
+
+//function myFunction(){
+ // var x= document.getElementById("mobilemenutoggle");
+ //  document.getElementsByClassName("gds-search__input").innerHTML= "You are searching for: " + x.value;
+//}
+
+ //var btn = document.querySelector('#mobilemenutoggle')
+//btn.addEventListener('click', (event) => {
+  //  (document.querySelector('.gds-search')).classList.toggle('active')
+ //(document.querySelector('.gds-search__input')).focus()
+//event.preventDefault();
+//})
+
+//const search = document.querySelector('.search')
+ //const btn = document.querySelector('.btn')
+//const input = document.querySelector('.input')
+// btn.addEventListener('click', () => {
+  //   (document.querySelector('.gds-search')).classList.toggle('active')
+// (document.querySelector('.gds-search__input')).focus()
+ //})
+
+ //alert("hi");*/
